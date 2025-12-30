ee7e3bb7cf738a581e1ba2175617a42c51ee6c93
Vipin Saini <vipinsaini0.me@gmail.com>
2025-12-30 06:13:47 +0530

    Initial Commit

---
+++ SecureDesk/SecureDeskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureDeskApp.swift
+//  SecureDesk
+//
+//  Created by Vipin Saini on 30/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SecureDeskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
